4d9afdb421241b526e4c4b568115c13c17bfcc6c
Mikeike34 <michaeljcantone@gmail.com>
2024-07-25 14:18:47 -0700

    Test code in main was fixed. Created a new object each time addItem was called.

--- CS131Lab3_MJC/ListApp.java
+++ CS131Lab3_MJC/ListApp.java
@@ -23,7 +23,24 @@ public class ListApp {
 		System.out.println(stringList.toString());
 		
 		System.out.println("\n====================testing addItem with squareList====================");
-		squareList.addItem(5);
+		squareList.addItem(new Square(5.0));
+		squareList.addItem(new Square(20.0));
+		squareList.addItem(new Square(18.0));
+		squareList.addItem(new Square(22.2));
+		squareList.addItem(new Square(19.3));
+		System.out.println(squareList.toString());
+		squareList.addItem(new Square(10.0));
+		System.out.println(squareList.toString());
+		
+		System.out.println("\n====================testing addItem with pointList====================");
+		pointList.addItem(new PointThreeD(10,22,82.4));
+		pointList.addItem(new PointThreeD(18.2,95,97));
+		pointList.addItem(new PointThreeD(1,8.6, 10));
+		pointList.addItem(new PointThreeD(20,50,55));
+		pointList.addItem(new PointThreeD(33.3,15.2,90.9));
+		System.out.println(pointList.toString());
+		pointList.addItem(new PointThreeD(1,2,3));
+		System.out.println(pointList.toString());
 		
 
 	}//end main
